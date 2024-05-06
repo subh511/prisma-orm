4ccfd145ce3061df87d5c7577190e0f2efd1d09d
subhranshu sekher dhal <ssdhal98@gmail.com>
2024-05-06 17:24:09 +0530

    added user details

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,26 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient()
+
+
+//prisma.user.create or
+//prisma.user.find or
+//prisma.user.delete or
+//we can do bunch of steps here
+
+
+async function insertUser(
+    username: string,
+    password: string,
+    firstname: string,
+    lastname: string
+){
+   const response = await prisma.user.create({
+        data: {
+            email: username,
+            password,
+            firstname,
+            lastname
+        }
+    })
+    console.log(response)
+}
